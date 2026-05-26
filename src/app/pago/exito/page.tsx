7c4f7afb0548222664a57ad3eabe1dd36a43d571
Kevin Gallardo <andreskevin2606@gmail.com>
2026-05-26 11:17:29 -0500

    fix: pago exitoso

--- src/app/pago/exito/page.tsx
+++ src/app/pago/exito/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { CheckoutSuccessClient } from "@/features/orders/CheckoutSuccessClient";
 
 export default function CheckoutSuccessPage() {
-  return <CheckoutSuccessClient />;
+  return (
+    <Suspense fallback={<div>Cargando...</div>}>
+      <CheckoutSuccessClient />
+    </Suspense>
+  );
 }
